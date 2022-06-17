5f24e8a8c7d170f4e9cda661e1423a7be9e27aa7
Julia Seward <giga.juwels@gmail.com>
2022-06-16 22:44:10 -0400

    create project

---
+++ WalletConnect Sharp V2/WalletConnectSharp.Network/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WalletConnectSharp.Network
+{
+    public class Class1
+    {
+    }
+}
---
+++ WalletConnect Sharp V2/WalletConnectSharp.Storage/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WalletConnectSharp.Storage
+{
+    public class Class1
+    {
+    }
+}
