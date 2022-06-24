0dccb06209f4642b02fee4ce41a133b0f79babff
Eddie Penta <ecp4224@gmail.com>
2022-06-24 19:59:03 -0400

    create interface

---
+++ WalletConnect Sharp V2/WalletConnectSharp.Storage/Interfaces/IKeyValueStorage.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Threading.Tasks;
+
+namespace WalletConnectSharp.Storage.Interfaces
+{
+    public interface IKeyValueStorage
+    {
+        Task<string[]> GetKeys();
+
+        Task<Tuple<string, T>[]> GetEntriesOfType<T>();
+
+        Task<object[]> GetEntries();
+
+        Task<T> GetItem<T>(string key);
+
+        Task SetItem<T>(string key, T value);
+
+        Task RemoveItem(string key);
+    }
+}
