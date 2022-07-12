17736e746112265e14a7ace4188ba37dfd34018a
Eddie Penta <ecp4224@gmail.com>
2022-07-11 21:33:55 -0400

    add docs

--- WalletConnect Sharp V2/WalletConnectSharp.Crypto/Interfaces/ICrypto.cs
+++ WalletConnect Sharp V2/WalletConnectSharp.Crypto/Interfaces/ICrypto.cs
@@ -4,12 +4,33 @@ using WalletConnectSharp.Network;
 
 namespace WalletConnectSharp.Crypto.Interfaces
 {
+    /// <summary>
+    /// A module that handles both key management and encoding/decoding of data with a given topic/keypair.
+    ///
+    /// A module holds one IKeyChain and stores generated keys in that keychain. The storage of generated keys
+    /// is handled by the IKeyChain.
+    /// </summary>
     public interface ICrypto : IModule
     {
+        /// <summary>
+        /// The IKeyChain this crypto module will use to store/retrieve key pairs
+        /// </summary>
         IKeyChain KeyChain { get; }
 
+        /// <summary>
+        /// Initialize the crypto module. This should initialize the current KeyCHain if present.
+        ///
+        /// This call is asynchronous, since initializing the IKeyChain requires asynchronous initialization.
+        /// </summary>
+        /// <returns>The crypto module initialization task</returns>
         Task Init();
 
+        /// <summary>
+        /// Check if a keypair with a given tag is stored in this crypto module. This should
+        /// check the backing keychain.
+        /// </summary>
+        /// <param name="tag">The tag of the keychain to look for</param>
+        /// <returns>A </returns>
         Task<bool> HasKeys(string tag);
 
         Task<string> GenerateKeyPair();
