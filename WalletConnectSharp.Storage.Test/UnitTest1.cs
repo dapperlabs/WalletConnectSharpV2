32f9addc4575a2e83adb02aef0b6ae7753411a5a
Julia Seward <giga.juwels@gmail.com>
2022-07-17 22:37:13 -0400

    Added unit test for storage module

---
+++ WalletConnectSharp.Storage.Test/UnitTest1.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace WalletConnectSharp.Storage.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        async public void GetSetRemoveTest()
+        {
+            var testDictStorage = new DictStorage();
+            await testDictStorage.SetItem("somekey", "somevalue");
+            Assert.Equal("somevalue",await testDictStorage.GetItem<string>("somekey"));
+            await testDictStorage.RemoveItem("somekey");
+            await Assert.ThrowsAsync<KeyNotFoundException>(() => testDictStorage.GetItem<string>("somekey"));
+        }
+
+        [Fact]
+        async public void GetKeysTest()
+        {
+            var testDictStorage = new DictStorage();
+            await testDictStorage.SetItem("addkey", "testingvalue");
+            Assert.Equal(new string[]{"addkey"}, await testDictStorage.GetKeys()); 
+        }
+
+        [Fact]
+        async public void GetEntriesTests()
+        {
+            var testDictStorage = new DictStorage();
+            await testDictStorage.SetItem("addkey", "testingvalue");
+            Assert.Equal(new object[]{"testingvalue"}, await testDictStorage.GetEntries());
+            await testDictStorage.SetItem("newkey", 5);
+            Assert.Equal(new int[]{5}, await testDictStorage.GetEntriesOfType<int>());
+
+        }
+    }
+    
+}
